913815e9159614d5be7acb3db33c99d933a6d257
Michael Clausen <michaeljclausen@gmail.com>
2017-06-20 10:25:41 -0700

    Adds styles for list items

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -3,9 +3,11 @@ import ListItem from './ListItem.jsx';
 
 const List = (props) => (
   <div>
-    <h4> List Component </h4>
+    <h4> **************** </h4>
     There are { props.items.length } items.
-    { props.items.map((item, i) => <ListItem item={item} key={i} />)}
+    <ul>
+      {props.items.map((item, i) => <ListItem item={item} key={i} />)}
+    </ul>
   </div>
 )
 
--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-const ListItem = (props) => (
-  <div>
-    {props.item.artist}
-  </div>
-)
-
+const ListItem = (props) => {
+  const likedStyle = {
+    color: 'green'
+  }
+  const dislikedStyle = {
+    color: 'red'
+  }
+  if (props.item.listened === 1) {
+    if (props.item.liked === 1) {
+      return (
+        <div>
+          <div style={likedStyle}>{props.item.artist} -- {props.item.album} -- {props.item.song} -- {props.item.first_name} liked this</div>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <div style={dislikedStyle}>{props.item.artist} -- {props.item.album} -- {props.item.song} -- {props.item.first_name} disliked this</div>
+        </div>
+      )
+    }
+  } else {
+    return (
+      <div>
+        <div>{props.item.artist} -- {props.item.album} -- {props.item.song} -- <button>listened</button></div>
+      </div>
+    )
+  }
+}
 export default ListItem;
